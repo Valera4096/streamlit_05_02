c5ff02960495cae0baf50c14d61c103ae3248a1f
Valera <Fyodorov.val99@gmail.com>
2024-01-30 18:24:38 +0300

    Добавил возможность вставлять фото

--- stream.py
+++ stream.py
@@ -1,32 +1,37 @@
 import streamlit as st
-import numpy as np 
-from skimage import io
+# import numpy as np 
+# from skimage import io
+# import matplotlib.pyplot as plt
+
+import numpy as np
 import matplotlib.pyplot as plt
+from skimage import io
+from skimage.color import rgb2gray
+from skimage.transform import resize
 
-age = st.slider('На сколько сжать фото?', 0, 666, 666)
 
-url = 'https://cs.pikabu.ru/post_img/big/2013/03/17/6/1363508611_1596589037.jpg'
+url = st.text_input('Вставьте URL ссылку с изображением', 'https://dobrovserdce.ru/images/2022/11/02/kot%20Fedya_large.jpeg')
 image = io.imread(url)
 
-U, sing_vals, V = np.linalg.svd(image)
+age = st.slider('На сколько сжать фото?', 0, 100, 50)
 
-sigma = np.zeros_like(image, dtype=np.float64)
+if len(image.shape) == 3:
+    gray_image = rgb2gray(image)
+else:
+    gray_image = image
+    
+U, sing_vals, V = np.linalg.svd(gray_image)
+sigma = np.zeros_like(gray_image, dtype=np.float64)
 
 np.fill_diagonal(sigma, sing_vals)
 
-top_k = age
+top_k = int(image.shape[0] - image.shape[0]/100 * age)
 
 trunc_U = U[:, :top_k]
 trunc_sigma = sigma[:top_k, :top_k]
 trunc_V = V[:top_k, :]
-
 trunc_image = trunc_U @ trunc_sigma @ trunc_V
-# fig, ax = plt.subplots(1, 2, figsize=(15, 20))
-# ax[0].imshow(image, cmap='gray')
-# ax[0].set_title('Исходное изобржение')
-# ax[1].imshow(trunc_image, cmap='gray')
-# ax[1].set_title(f'Изображение на top {top_k} сингулярных чисел');
-# st.pyplot(fig)
+
 fig1, ax1 = plt.subplots(figsize=(15, 20))
 ax1.imshow(image, cmap='gray')
 
@@ -35,4 +40,7 @@ ax2.imshow(trunc_image, cmap='gray')
 st.pyplot(fig1)
 st.pyplot(fig2)
 Proch = 1 - age/666
-st.write(f'Фото сжато на {int(Proch * 100)} %')
+st.write(f'Фото сжато на {age} %')
+
+
+
